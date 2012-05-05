18c623e83c728445a3930f17e231a738e5a5cb79
Shish <shish@shishnet.org>
2012-05-05 01:45:54 +0100

    working world spawner

--- src/world.erl
+++ src/world.erl
@@ -10,12 +10,15 @@
 
 init(_Opts) ->
 	io:format("Loading World~n", []),
-	Zones = gb_trees:empty(),
-	Zones0 = gb_trees:insert("Home Zone", zone:start_link("Home Zone"), Zones),
-	{ok, #world{zones=Zones0}}.
+	Zones0 = gb_trees:empty(),
+	Zones1 = gb_trees:insert("Home_Zone",   zone:start_link("Home Zone"),   Zones0),
+	Zones2 = gb_trees:insert("Pirate_Zone", zone:start_link("Pirate Zone"), Zones1),
+	Zones3 = gb_trees:insert("FFA_Zone",    zone:start_link("FFA Zone"),    Zones2),
+	Zones4 = gb_trees:insert("Trade_Zone",  zone:start_link("Trade Zone"),  Zones3),
+	{ok, #world{zones=Zones4}}.
 
 
-handle_call({getZone, Name}, _From, State) ->
+handle_call({get_zone, Name}, _From, State) ->
 	{reply, gb_trees:get(Name, State#world.zones), State};
 
 handle_call(Message, _From, State) ->
@@ -51,4 +54,4 @@ start_link() ->
 	Pid.
 
 get_zone(ZoneName) ->
-	gen_server:call(world, {getZone, ZoneName}).
+	gen_server:call(world, {get_zone, ZoneName}).
