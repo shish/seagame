7f59909d96a80b116a4adb1788664cd9bb161b04
Shish <shish@shishnet.org>
2012-05-08 13:00:38 +0100

    separate exports for gen_server and api

--- src/authdb.erl
+++ src/authdb.erl
@@ -1,11 +1,8 @@
 -module(authdb).
 -behaviour(gen_server).
 -include("records.hrl").
--export([
-	init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3,
-	start_link/0, test/0,
-	get_user/2
-	]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+-export([start_link/0, test/0, get_user/2]).
 
 
 % gen_server API
