9dc2dc42722b95026330c2bc37d05354a29ad718
Shish <shish@shishnet.org>
2012-05-16 16:00:54 +0100

    stub http front end

--- src/http_front_end.erl
+++ src/http_front_end.erl
@@ -1,5 +1,13 @@
 -module(http_front_end).
--export([start_link/1]).
+-export([start_link/1, start_http_server/1]).
 
-start_link(_Port) ->
-	{ok, not_implemented}.
+start_link(Port) ->
+	Pid = spawn_link(http_front_end, start_http_server, [Port]),
+	{ok, Pid}.
+
+start_http_server(Port) ->
+	io:format("Waiting for incoming requests~n"),
+	receive
+		_ -> true
+	end,
+	start_http_server(Port).
