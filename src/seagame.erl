7f10a91966694549411ceed637bf778cec0a4c99
Shish <shish@shishnet.org>
2012-05-16 16:01:19 +0100

    make seagame an application and a supervisor, rather than just a hack

--- src/seagame.erl
+++ src/seagame.erl
@@ -1,36 +1,21 @@
 -module(seagame).
--include("records.hrl").
--export([start/0]).
+-behaviour(application).
+-behaviour(supervisor).
+-export([start/2, stop/1, init/1]).
 
--record(universe, {world_pid, authdb_pid, tcp_fe_pid, http_fe_pid}).
+start(normal, _Args) ->
+	supervisor:start_link({local, ?MODULE}, ?MODULE, {}).
 
+stop(_State) ->
+	ok.
 
-start() ->
-	process_flag(trap_exit, true),
-	{ok, WorldPid} = world:start_link(),
-	{ok, AuthDBPid} = authdb:start_link(),
-	{ok, TCPPid} = tcp_front_end:start_link(1234),
-	{ok, HTTPPid} = http_front_end:start_link(8000),
-	loop(#universe{world_pid=WorldPid, authdb_pid=AuthDBPid, tcp_fe_pid=TCPPid, http_fe_pid=HTTPPid}).
-
-
-loop(Universe) ->
-	{universe, WorldPid, AuthDBPid, TCPPid, HTTPPid} = Universe,
-	receive
-		{'EXIT', WorldPid, Reason} ->
-			io:format("WARN: World exited with reason: ~p~n", [Reason]),
-			{ok, NewWorldPid} = world:start_link(),
-			loop(Universe#universe{world_pid=NewWorldPid});
-		{'EXIT', AuthDBPid, Reason} ->
-			io:format("WARN: AuthDB exited with reason: ~p~n", [Reason]),
-			{ok, NewAuthDBPid} = authdb:start_link(),
-			loop(Universe#universe{authdb_pid=NewAuthDBPid});
-		{'EXIT', TCPPid, Reason} ->
-			io:format("WARN: TCP Frontend exited with reason: ~p~n", [Reason]),
-			{ok, NewTCPPid} = world:start_link(),
-			loop(Universe#universe{tcp_fe_pid=NewTCPPid});
-		{'EXIT', HTTPPid, Reason} ->
-			io:format("WARN: HTTP Frontend exited with reason: ~p~n", [Reason]),
-			{ok, NewHTTPPid} = world:start_link(),
-			loop(Universe#universe{http_fe_pid=NewHTTPPid})
-	end.
+init(_) ->
+	{ok, {
+		{one_for_one, 3, 60},
+		[
+			{world,          {world, start_link, []},              permanent, 1000, worker, [world]},
+			{authdb,         {authdb, start_link, []},             permanent, 1000, worker, [authdb]},
+			{tcp_front_end,  {tcp_front_end, start_link, [1234]},  permanent, 1000, worker, [tcp_front_end]},
+			{http_front_end, {http_front_end, start_link, [8000]}, permanent, 1000, worker, [http_front_end]}
+		]
+	}}.
